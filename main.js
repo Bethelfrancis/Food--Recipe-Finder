5539773d8da723847ecef24d96924afc1fc4880d
Bethelkid <bethelndegonekwu@gmail.com>
2024-09-22 21:43:48 +0100

    Initial Commit

--- main.js
+++ main.js
@@ -1,52 +1,99 @@
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
-const pokemonName = document.getElementById('pokemon-name');
-const pokemonId = document.getElementById('pokemon-id');
-const pokemonWeight = document.getElementById('weight');
-const pokemonHeight = document.getElementById('height');
-const pokemonTypes = document.getElementById('types');
-const hp = document.getElementById('hp');
-const attack = document.getElementById('attack');
-const defense = document.getElementById('defense');
-const specialAttack = document.getElementById('special-attack');
-const specialDefense = document.getElementById('special-defense');
-const speed = document.getElementById('speed');
-const spritesContainer = document.getElementById('sprites-container');
+const foodContainer = document.querySelector('.food-container');
+const loadMoreBtn = document.getElementById('load-more');
+const hambuger = document.querySelector('.hambuger');
+const mobileNav = document.querySelector('.nav-links');
+const navBtn = document.querySelectorAll('.nav-link-btn');
 
-const fetchData = async () => {
+let startingIndex = 0;
+let endingIndex = 4;
+let allFoods = []
+
+const fetchFoodData = async () => {
   try {
-    const pokemonNameOrId = searchInput.value.toLowerCase();
-    const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`);
+    const foodName = searchInput.value;
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
     const data = await res.json();
 
-    pokemonName.textContent = `${data.name.toUpperCase()}`
-    pokemonId.textContent = `#${data.id}`
-    pokemonWeight.textContent = `Weight: ${data.weight}`
-    pokemonHeight.textContent = `Height: ${data.height}`
+    if (!searchInput.value) {
+      return alert('Input a value');
+    }
 
-    spritesContainer.innerHTML = `<img id="sprite" src="${data.sprites.front_default}" alt="${data.name}" />`
+    if (!data.meals) {
+      return foodContainer.innerHTML = `<p>Food not found.</p>`;
+    }
 
-    hp.textContent = `HP: ${data.stats[0].base_stat}`
-    attack.textContent = `Attack: ${data.stats[1].base_stat}`
-    defense.textContent = `Defense: ${data.stats[2].base_stat}`
-    specialAttack.textContent = `Special Attack: ${data.stats[3].base_stat}`
-    specialDefense.textContent = `Special Defence: ${data.stats[4].base_stat}`
-    speed.textContent = `Speed: ${data.stats[5].base_stat}`
+    allFoods = data.meals;
+    displayFoods(startingIndex, endingIndex);
 
-    pokemonTypes.innerHTML = data.types.map(obj => `<span>${obj.type.name.toUpperCase()}</span>`).join('')
+    if (allFoods.length > endingIndex) {
+      loadMoreBtn.style.display = 'block';
+    } else {
+      loadMoreBtn.style.display = 'none'
+    }
+
   } catch (err) {
-    alert('Pokémon not found');
-    console.log(`Pokémon not found: ${err}`);
+    console.log(err);
+    alert('An error occurred while fetching the food data');
   }
-};
+}
 
-searchButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  fetchData()
+const displayFoods = (start, end) => {
+  foodContainer.innerHTML += allFoods.slice(startingIndex, endingIndex)
+      .map(food => {
+        return `
+          <div class="food-item">
+            <div class="food-image">
+              <img src="${food.strMealThumb}" alt="${food.strMeal}" />
+            </div>
+            <div class="food-details">
+              <h3>${food.strMeal}</h3>
+              <p class="food-country">Country: ${food.strArea}</p>
+              <div class="recipe-links">
+                <a href="${food.strSource}" target="_blank" class="recipe-btn">View Recipe</a>
+                <iframe src="${food.strYoutube.replace("watch?v=", "embed/")}" frameborder="0" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+        `
+      })
+      .join('');
+}
+
+hambuger.addEventListener('click', () => {
+  hambuger.classList.toggle('open');
+  mobileNav.classList.toggle('show');
 })
 
-searchInput.addEventListener('keydown', (e) => {
+navBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    hambuger.classList.remove('open');
+    mobileNav.classList.remove('show');
+  })
+})
+
+searchButton.addEventListener('click', e => {
+  e.preventDefault();
+  startingIndex = 0
+  endingIndex = 4;
+  foodContainer.innerHTML = '';
+  fetchFoodData();
+})
+
+searchInput.addEventListener('keydown', e => {
   if (e.key === 'Enter') {
-    searchButton.click()
+    searchButton.click();
+  }
+})
+
+loadMoreBtn.addEventListener('click', () => {
+  startingIndex += 4;
+  endingIndex += 4;
+
+  displayFoods(startingIndex, endingIndex);
+
+  if (endingIndex >= allFoods.length) {
+    loadMoreBtn.style.display = 'none';
   }
 })
